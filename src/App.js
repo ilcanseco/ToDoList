498360e6e3dd8b90f94622fe28d22df6edb10b97
Ivan <ilcanseco97@gmail.com>
2019-04-19 01:06:56 -0400

    Added a textbox

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
       todos: todosData
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   handleChange(id) {
@@ -27,12 +28,36 @@ class App extends Component {
     });
   }
 
+  handleKeyDown = e => {
+    if (e.key === "Enter") {
+      this.setState({
+        todos: [this.state.todos, this.makeItem()]
+      });
+      console.log(this.state.todos);
+    }
+  };
+
   render() {
     const todoItems = this.state.todos.map(item => (
       <ToDoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
 
-    return <div className="todo-list">{todoItems}</div>;
+    return (
+      <div className="todo-list">
+        {todoItems}
+        {
+          <form>
+            <input
+              className="text-box"
+              type="text"
+              placeholder="Enter a new task"
+              value={this.state.text}
+              onKeyDown={this.handleKeyDown}
+            />
+          </form>
+        }
+      </div>
+    );
   }
 }
 
